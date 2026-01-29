540584a5eabe040da08081eb2087045cfa39916d
xiahfei <124034214+xiahfei@users.noreply.github.com>
2026-01-29 08:24:49 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -48,7 +48,7 @@ UPDATE_PACKAGE() {
 #UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
-UPDATE_PACKAGE "linkease" "linkease/nas-packages-luci/tree/main/luci/luci-app-linkease"
+UPDATE_PACKAGE "linkease" "linkease/nas-packages-luci/luci" "main" "tree" "luci-app-linkease"
 #UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base qt6tools rblibtorrent"
 
 
